a56fb9175ed5da59a69d569e162dcb40ff28f630
Dmitry Novosyolov <i1710@icloud.com>
2019-08-23 11:31:00 +0300

    Initial Commit

---
+++ SwiftUI Core Data/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Core Data
+//
+//  Created by Dmitry Novosyolov on 23/08/2019.
+//  Copyright © 2019 Dmitry Novosyolov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
